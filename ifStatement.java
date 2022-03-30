a09e9058ebb080ad35c3525526bd5b6d03bb02e5
asifs-cse <asifs.cse@gmail.com>
2022-03-30 22:25:08 +0530

    if condition

--- ifStatement.java
+++ ifStatement.java
@@ -1,7 +1,15 @@
 package ControlStatement;
+import java.util.Scanner;
 
 public class ifStatement {
     public static void main(String args[]){
-        System.out.print("Hello world");
+        Scanner input = new Scanner(System.in);
+        int age;
+        System.out.print("Enter your age: ");
+        age=input.nextInt();
+
+        if(age>=18){
+            System.out.println("You are eligable for vote.");
+        }
     }
 }
