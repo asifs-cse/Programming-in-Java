8653dcf31c2963722537873a5529db3e5a8cf862
asifs-cse <asifs.cse@gmail.com>
2022-04-01 23:39:19 +0530

    create variable

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -2,7 +2,7 @@ package ControlStatement;
 import java.util.Scanner;
 public class TernaryOperator {
     public static void main(String args[]){
-        int num;
+        int num; //create variable
         Scanner input = new Scanner(System.in);
         num = input.nextInt();
         System.out.println(num%2==0? "Ever numer":"Odd number");
