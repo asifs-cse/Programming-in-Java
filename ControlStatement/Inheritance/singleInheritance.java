6a862fc3a03391a1677af348792ea1cc61eac63f
Asif Shahriar <asifs.cse@gmail.com>
2022-05-16 10:39:09 +0530

    single inheritance

--- ControlStatement/Inheritance/singleInheritance.java
+++ ControlStatement/Inheritance/singleInheritance.java
@@ -1,14 +1,14 @@
 package ControlStatement.Inheritance;
 class Animal{
-    void eat(){System.out.println("Eating...");}
+    void eat(){System.out.println("Eating...");} //base class method
 }
 class Dog extends Animal{
-    void bark(){System.out.println("barking...");}
+    void bark(){System.out.println("barking...");} //sub class method
 }
 public class singleInheritance {
     public static void main(String args[]){
-        Dog d = new Dog();
-        d.bark();
-        d.eat();
+        Dog d = new Dog(); //create object
+        d.bark(); //class sub class method
+        d.eat(); //call base class method
     }
 }
